faa0a13475356e5975153e615bb671b2031404f3
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2015-01-30 09:56:44 +0100

    Changes to install OptimPack library with autotools

--- deps/build.jl
+++ deps/build.jl
@@ -1,32 +1,36 @@
 using BinDeps
 
-const urioptimpack = URI("https://github.com/emmt/OptimPack/archive/master.zip")
+const version = "master"
+const unpacked_dir = "OptimPack-$version"
 
 @BinDeps.setup
 
-optimpack = library_dependency("libOptimPack2")
-provides(Sources,urioptimpack,optimpack,unpacked_dir="OptimPack-master")
+optimpack = library_dependency("libopk")
 
-prefix = joinpath(BinDeps.depsdir(optimpack),"usr")
-srcdir = joinpath(BinDeps.depsdir(optimpack),"src","OptimPack-master/src")
-@unix_only libfilename = "libOptimPack2.so"
-@osx_only libfilename = "libOptimPack2.dylib"
-destlib = joinpath(prefix,"lib",libfilename)
+provides(Sources,
+         URI("https://github.com/emmt/OptimPack/archive/$version.zip"),
+         optimpack,
+         unpacked_dir=unpacked_dir)
+
+prefix = joinpath(BinDeps.depsdir(optimpack), "usr")
+srcdir = joinpath(BinDeps.depsdir(optimpack), "src", unpacked_dir)
 
 provides(SimpleBuild,
          (@build_steps begin
-            GetSources(optimpack)
-            CreateDirectory(prefix)
-            CreateDirectory(joinpath(prefix,"lib"))
-            @build_steps begin
-                ChangeDirectory(srcdir)
-                FileRule(destlib,
-                        @build_steps begin
-                        `make`
-                        `cp libOptimPack2.so.1.0.0 $destlib`
-                        end)
-            end
+             GetSources(optimpack)
+             CreateDirectory(prefix)
+             CreateDirectory(joinpath(prefix,"lib"))
+             @build_steps begin
+                 ChangeDirectory(srcdir)
+                 FileRule(destlib,
+                          @build_steps begin
+                              `./autogen.sh`
+                              `./configure --enable-shared --disable-static --prefix="$prefix"`
+                              `make`
+                              `make install`
+                          end)
+             end
          end),
          optimpack)
 
-@BinDeps.install [:libOptimPack2 => :opklib]
+@BinDeps.install [:libopk => :opklib]
