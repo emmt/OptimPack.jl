78481309e5e529258975ba9d107713a2b3211dec
Antony Schutz <antonyschutz@gmail.com>
2016-11-02 16:50:49 +0100

    @unix/osx_only replaced by test @static

--- deps/build.jl
+++ deps/build.jl
@@ -14,8 +14,10 @@ provides(Sources,
 
 prefix = joinpath(BinDeps.depsdir(optimpack), "usr")
 srcdir = joinpath(BinDeps.depsdir(optimpack), "src", unpacked_dir)
-@unix_only libfilename = "libopk.so"
-@osx_only libfilename = "libopk.dylib"
+@static if is_unix() libfilename = "libopk.so"
+end
+@static if is_apple() libfilename = "libopk.dylib"
+end
 destlib = joinpath(prefix, "lib", libfilename)
 
 provides(SimpleBuild,
