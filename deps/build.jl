a7ee96771ecbeb5c6d726f1b9fd6cc0446ccc0c7
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2017-05-26 18:22:09 +0200

    Fix determination of dynamic library extension

--- deps/build.jl
+++ deps/build.jl
@@ -16,17 +16,19 @@ provides(Sources,
 prefix = joinpath(BinDeps.depsdir(optimpack), "usr")
 srcdir = joinpath(BinDeps.depsdir(optimpack), "src", unpacked_dir)
 libdir = joinpath(prefix, "lib")
-name = "opk"
-@compat @static if is_unix()
-    libfilename = "lib$(name).so"
-elseif is_apple()
-    libfilename = "lib$(name).dylib"
-#elseif is_windows()
-#    libfilename = "$(name).dll"
-else
-    error("unknown architecture")
+
+function dynlibname(name)
+    ext = Base.Libdl.dlext
+    @compat @static if is_unix()
+        return "lib$(name).$(ext)"
+    elseif is_windows()
+        return "$(name).$(ext)"
+    else
+        error("unknown architecture")
+    end
 end
-destlib = joinpath(libdir, libfilename)
+
+destlib = joinpath(libdir, dynlibname("opk"))
 
 provides(SimpleBuild,
          (@build_steps begin
