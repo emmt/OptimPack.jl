b8e0e48754c03a1b55ccd5439c0d85c120c4d9e5
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2017-06-13 11:17:30 +0200

    Fix optimize method

--- src/bobyqa.jl
+++ src/bobyqa.jl
@@ -106,7 +106,7 @@ function optimize!(f::Function, x::DenseVector{Cdouble},
 end
 
 optimize(f::Function, x0::DenseVector{Cdouble}, args...; kwds...) =
-    optimize(f, copy(x0), args...; kwds...)
+    optimize!(f, copy(x0), args...; kwds...)
 
 minimize!(args...; kwds...) = optimize!(args...; maximize=false, kwds...)
 maximize!(args...; kwds...) = optimize!(args...; maximize=true, kwds...)
