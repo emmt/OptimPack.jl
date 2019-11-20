137e643e5664abea17bc5a32f1e7b8ec91543851
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2019-11-20 17:39:45 +0100

    Add benchmark tests

---
+++ test/benchmarks.jl
@@ -0,0 +1,123 @@
+#
+# benchmarks.jl --
+#
+# Perform a few benchmark tests on OptimPack arrays.
+#
+# ----------------------------------------------------------------------------
+#
+# This file is part of OptimPack.jl which is licensed under the MIT "Expat"
+# License:
+#
+# Copyright (C) 2014-2019, Éric Thiébaut.
+#
+# ----------------------------------------------------------------------------
+
+
+module OptimPackBenchmarks
+
+using OptimPack
+using BenchmarkTools
+
+function slowvnorm2(x::AbstractArray{T,N}) where {T<:AbstractFloat,N}
+    res = zero(T)
+    for i in eachindex(x)
+        res += x[i]^2
+    end
+    return sqrt(res)
+end
+
+function slowvdot(x::AbstractArray{T,N},
+                  y::AbstractArray{T,N}) where {T<:AbstractFloat,N}
+    @assert axes(x) == axes(y)
+    res = zero(T)
+    for i in eachindex(x, y)
+        res += x[i]*y[i]
+    end
+    return res
+end
+
+function slowvcopy!(dst::AbstractArray{T,N},
+                    src::AbstractArray{T,N}) where {T,N}
+    @assert axes(dst) == axes(src)
+    for i in eachindex(dst, src)
+        dst[i] = src[i]
+    end
+    return dst
+end
+
+function vnorm2(x::AbstractArray{T,N}) where {T<:AbstractFloat,N}
+    res = zero(T)
+    @inbounds @simd for i in eachindex(x)
+        res += x[i]^2
+    end
+    return sqrt(res)
+end
+
+function vdot(x::AbstractArray{T,N},
+              y::AbstractArray{T,N}) where {T<:AbstractFloat,N}
+    @assert axes(x) == axes(y)
+    res = zero(T)
+    @inbounds @simd for i in eachindex(x, y)
+        res += x[i]*y[i]
+    end
+    return res
+end
+
+function vcopy!(dst::AbstractArray{T,N},
+                src::AbstractArray{T,N}) where {T,N}
+    @assert axes(dst) == axes(src)
+    @inbounds @simd for i in eachindex(dst, src)
+        dst[i] = src[i]
+    end
+    return dst
+end
+
+T = Float32
+dims = (20,30,40)
+len = prod(dims)
+
+x = randn(T, dims)
+y = randn(T, dims)
+z = randn(T, dims)
+
+E = OptimPack.DenseVariableSpace(T, dims)
+vx = vcopy!(OptimPack.create(E), x)
+vy = vcopy!(OptimPack.create(E), y)
+vz = vcopy!(OptimPack.create(E), z)
+
+@assert vnorm2(vz) == vnorm2(z)
+@assert vdot(vx,vy) == vdot(x,y)
+
+println("\nSlow versions (with bound checking):")
+println("  VNORM2 of $len elements:")
+print("    Julia arrays ------------>")
+@btime slowvnorm2($z)
+print("    DenseVariable arrays ---->")
+@btime slowvnorm2($vz)
+println("  VDOT of $len elements:")
+print("    Julia arrays ------------>")
+@btime slowvdot($x,$y)
+print("    DenseVariable arrays ---->")
+@btime slowvdot($vx,$vy)
+print("    mixed type arrays ------->")
+@btime slowvdot($x,$vy)
+print("    mixed type arrays ------->")
+@btime slowvdot($vx,$y)
+
+println("\nfast versions (np bound checking and SIMD):")
+println("  VNORM2 of $len elements:")
+print("    Julia arrays ------------>")
+@btime vnorm2($z)
+print("    DenseVariable arrays ---->")
+@btime vnorm2($vz)
+println("  VDOT of $len elements:")
+print("    Julia arrays ------------>")
+@btime vdot($x,$y)
+print("    DenseVariable arrays ---->")
+@btime vdot($vx,$vy)
+print("    mixed type arrays ------->")
+@btime vdot($x,$vy)
+print("    mixed type arrays ------->")
+@btime vdot($vx,$y)
+
+end
