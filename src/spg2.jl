56b7cc7dbace345db1bf83191691cc24d211930b
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2019-11-23 13:49:00 +0100

    Fix sign bug and use new API

--- src/spg2.jl
+++ src/spg2.jl
@@ -22,7 +22,7 @@
 #
 # ----------------------------------------------------------------------------
 
-struct SpgResult{T,N}
+mutable struct SpgResult{T,N}
     x::DenseArray{T,N}
     xbest::DenseArray{T,N}
     f::Float64
@@ -34,49 +34,59 @@ struct SpgResult{T,N}
     iter::Int
     status::Symbol
     #(::Type{SpgResult(x::DenseArray{T,N}}){T,N}, xbest::DenseArray{T,N}) =
-    #    new{T,N}(x, xbest, 0.0, 0.0, 0.0, 0.0, 0, 0, 0, :WORK_IN_PROGRESS)
+    #    new{T,N}(x, xbest, 0.0, 0.0, 0.0, 0.0, 0, 0, 0, :SEARCHING)
 end
 SpgResult(x::DenseArray{T,N}, xbest::DenseArray{T,N}) where {T,N} =
-    SpgResult{T,N}(x, xbest, 0.0, 0.0, 0.0, 0.0, 0, 0, 0, :WORK_IN_PROGRESS)
+    SpgResult{T,N}(x, xbest, 0.0, 0.0, 0.0, 0.0, 0, 0, 0, :SEARCHING)
 
-function spg2(fg!::Function, prj!::Function,
-              x0::DenseArray{T,N}, m::Integer;
-              maxit::Integer=-1, maxfc::Integer=-1,
-              eps1::Real=1e-6, eps2::Real=1e-6,
-              eta::Real=1.0, ftol::Real=1e-4,
-              lmin::Real=1e-30, lmax::Real=1e+30,
-              amin::Real=0.1, amax::Real=0.9,
-              printer=nothing, verb::Bool=false) where {T,N}
+function spg2(fg!::Function,
+              prj!::Function,
+              x0::DenseArray{T,N},
+              m::Integer = 10;
+              maxit::Integer = typemax(Int),
+              maxfc::Integer = typemax(Int),
+              eps1::Real = 1e-6,
+              eps2::Real = 1e-6,
+              eta::Real = 1.0,
+              ftol::Real = 1e-4,
+              lmin::Real = 1e-30,
+              lmax::Real = 1e+30,
+              amin::Real = 0.1,
+              amax::Real = 0.9,
+              printer = nothing,
+              verb::Bool = false) where {T<:AbstractFloat,N}
 
     # Allocate workspace.
     x0 = copy(x0)
     dims = size(x0)
-    space = DenseVectorSpace(T, dims)
+    space = DenseVariableSpace(T, dims)
     d = Array{T}(undef, dims)
     g = Array{T}(undef, dims)
     x = Array{T}(undef, dims)
     g = Array{T}(undef, dims)
     g0 = Array{T}(undef, dims)
     xbest = Array{T}(undef, dims)
-    _x = wrap(space, x)
-    _g = wrap(space, g)
-    _d = wrap(space, d)
-    _x0 = wrap(space, x0)
-    _g0 = wrap(space, g0)
-    _xbest = wrap(space, xbest)
+
+    # Wrap OptimPack variables over Julia workspace arrays.
+    Vx = wrap(space, x)
+    Vg = wrap(space, g)
+    Vd = wrap(space, d)
+    Vx0 = wrap(space, x0)
+    Vg0 = wrap(space, g0)
 
     # Initialization.
     ws = SpgResult(x, xbest)
     local sty, sts, f0, f
     if m > 1
         lastfv = Array{T}(undef, m)
-        fill!(lastfv, inf(T))
+        fill!(lastfv, T(Inf))
     else
         lastfv = nothing
     end
 
     # Project initial guess.
-    prj!(x, x0)
+    prj!(x0, x0)
+    copyto!(x, x0)
     ws.pcnt += 1
 
     # Evaluate function and gradient.
@@ -85,45 +95,46 @@ function spg2(fg!::Function, prj!::Function,
 
     # Initialize best solution and best function value.
     ws.fbest = f
-    copyto!(_xbest, _x)
+    copyto!(xbest, x)
 
     # Main loop.
     while true
 
-        # Compute continuous projected gradient (and its norms).
-        axpby!(_d, 1.0, _x, eta, _g)
-        prj!(_d, _d)
-        axpby!(_d, 1.0/eta, _x, -1.0/eta, _d)
+        # Compute continuous projected gradient (and its norms)
+        # as: `pg = (x - prj(x - eta*g))/eta`.
+        combine!(Vd, 1, Vx, -eta, Vg)
+        prj!(d, d)
+        combine!(Vd, 1/eta, Vx, -1/eta, Vd)
         ws.pcnt += 1
-        ws.pgtwon = norm2(_d)
-        ws.pginfn = norminf(_d)
+        ws.pgtwon = norm2(Vd)
+        ws.pginfn = norminf(Vd)
 
         # Print iteration information
-        if printer <: Function
+        if printer !== nothing
             printer(ws)
         end
         if verb
             @printf("ITER = %-5d  EVAL = %-5d  PROJ = %-5d  F(%s) =%24.17e  ||PG||oo = %17.10e\n",
-                    ws.iter, ws.fcnt, ws.pcnt, (f <= ws.fbest ? "+" : "-"), f, ws.pginfn)
+                    ws.iter, ws.fcnt, ws.pcnt, (f ≤ ws.fbest ? "+" : "-"), f, ws.pginfn)
         end
 
         # Test stopping criteria.
-        if ws.pginfn <= eps1
+        if ws.pginfn ≤ eps1
             # Gradient infinite-norm stopping criterion satisfied, stop.
-            ws.status = :CONVERGENCE_WITH_INFNORM
+            ws.status = :INFNORM_CONVERGENCE
             return ws
         end
-        if ws.pgtwon <= eps2
+        if ws.pgtwon ≤ eps2
             # Gradient 2-norm stopping criterion satisfied, stop.
-            ws.status = :CONVERGENCE_WITH_TWONORM
+            ws.status = :TWONORM_CONVERGENCE
             return ws
         end
-        if maxit >= 0 && ws.iter >= maxit
+        if ws.iter ≥ maxit
             # Maximum number of iterations exceeded, stop.
             ws.status = :TOO_MANY_ITERATIONS
             return ws
         end
-        if maxfc >= 0 && ws.fcnt >= maxfc
+        if ws.fcnt ≥ maxfc
             # Maximum number of function evaluations exceeded, stop.
             ws.status = :TOO_MANY_EVALUATIONS
             return ws
@@ -139,18 +150,18 @@ function spg2(fg!::Function, prj!::Function,
         end
 
         # Compute spectral steplength.
-        if iter == 0
-            # Initial steplength.
-            lambda = min(lmax, max(lmin, 1.0/ws.pginfn))
+        if ws.iter == 0
+            # Initial steplength. (FIXME: check type stability)
+            lambda = min(lmax, max(lmin, 1/ws.pginfn))
         else
-            _s = _x0 # alias
-            _y = _g0 # alias
-            axpby!(_s, 1.0, _x, -1.0, _x0)
-            axpby!(_y, 1.0, _g, -1.0, _g0)
-            sty = dot(_s, _y)
-            if sty > 0.0
+            Vs = Vx0 # alias
+            Vy = Vg0 # alias
+            combine!(Vs, 1, Vx, -1, Vx0)
+            combine!(Vy, 1, Vg, -1, Vg0)
+            sty = dot(Vs, Vy)
+            if sty > 0
                 # Safeguarded Barzilai & Borwein spectral steplength.
-                sts = dot(_s, _s)
+                sts = dot(Vs, Vs)
                 lambda = min(lmax, max(lmin, sts/sty))
             else
                 lambda = lmax
@@ -158,16 +169,16 @@ function spg2(fg!::Function, prj!::Function,
         end
 
         # Save current point.
-        copyto!(_x0, _x)
-        copyto!(_g0, _g)
+        copyto!(x0, x)
+        copyto!(g0, g)
         f0 = f
 
         # Compute the spectral projected gradient direction and <G,D>
-        axpby!(_x, 1.0, _x, -lambda, _g)
-        prj(_x, _x)
+        combine!(Vx, 1, Vx, -lambda, Vg)
+        prj!(x, x)
         ws.pcnt += 1
-        axpby!(_d, 1.0, _x, -1.0, _x0)
-        delta = dot(_g0, _d)
+        combine!(Vd, 1, Vx, -1, Vx0) # d = x - x0
+        delta = dot(Vg0, Vd)
 
         # Nonmonotone line search.
         stp = 1.0 # Step length for first trial.
@@ -181,15 +192,15 @@ function spg2(fg!::Function, prj!::Function,
             # corresponding best point.
             if f < ws.fbest
                 ws.fbest = f
-                copyto!(_xbest, _x)
+                copyto!(xbest, x)
             end
 
             # Test stopping criteria.
-            if f <= fmax + stp*ftol*delta
+            if f ≤ fmax + stp*ftol*delta
                 # Nonmonotone Armijo-like stopping criterion satisfied, stop.
                 break
             end
-            if maxfc >= 0 && ws.fcnt >= maxfc
+            if ws.fcnt ≥ maxfc
                 # Maximum number of function evaluations exceeded, stop.
                 ws.status = :TOO_MANY_EVALUATIONS
                 return ws
@@ -197,18 +208,18 @@ function spg2(fg!::Function, prj!::Function,
 
             # Safeguarded quadratic interpolation.
             q = -delta*(stp*stp)
-            r = (f - f0 - stp*delta)*2.0
-            if r > 0.0 && amin*r <= q <= amax*stp*r
+            r = (f - f0 - stp*delta)*2
+            if r > 0 && amin*r ≤ q ≤ amax*stp*r
                 stp = q/r
             else
-                stp /= 2.0
+                stp /= 2
             end
 
             # Compute trial point.
-            axpby!(_x, 1.0, _x0, stp, _d)
+            combine!(Vx, 1, Vx0, stp, Vd) # x = x0 + stp*d
         end
 
-        if ws.status != :WORK_IN_PROGRESS
+        if ws.status != :SEARCHING
             # The number of function evaluations was exceeded inside the line
             # search.
             return ws
