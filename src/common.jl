6424d4bebcef7e4d7b6db8b11dcb50e02e4cec4b
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2025-11-29 21:04:53 +0100

    Add missing `print_seconds`

--- src/common.jl
+++ src/common.jl
@@ -21,14 +21,27 @@ function ordinal_suffix(n::Integer)
     return "th"
 end
 
+function print_seconds(io::IO, t::Real)
+    a = abs(t)
+    if a*1e9 < 1e3
+        print(io, round(t*1e9; digits=3, base = 10), " ns")
+    elseif a*1e6 < 1e3
+        print(io, round(t*1e6; digits=3, base = 10), " μs")
+    elseif a*1e3 < 1e3
+        print(io, round(t*1e3; digits=3, base = 10), " ms")
+    else
+        print(io, round(t; digits=3, base = 10), " s")
+    end
+end
+
 @noinline throw_bad_argument(msg::AbstractString) = throw(ArgumentError(msg))
 @noinline throw_bad_argument(args...) = throw_bad_argument(string(args...))
 
 @noinline throw_dimension_mismatch(msg::AbstractString) = throw(DimensionMismatch(msg))
 @noinline throw_dimension_mismatch(args...) = throw_dimension_mismatch(string(args...))
 
-@noinline throw_incompatible_axes() = throw(DimensionMismatch(
-    "array arguments have incompatible axes"))
+@noinline throw_incompatible_axes() =  throw_dimension_mismatch(
+    "array arguments have incompatible axes")
 
 @noinline throw_assertion_failed(msg::AbstractString) = throw(AssertionError(msg))
 @noinline throw_assertion_failed(args...) = throw_assertion_failed(string(args...))
